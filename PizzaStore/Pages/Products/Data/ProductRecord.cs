f45e2509fc4acdcfc86d6f3ef8480d628b8a7896
WhitePhoenix-main <enix.main@gmail.com>
2022-08-13 00:01:11 +0800

    * Added new pages for products

--- PizzaStore/Pages/Products/Data/ProductRecord.cs
+++ PizzaStore/Pages/Products/Data/ProductRecord.cs
@@ -6,7 +6,7 @@ namespace PizzaStore
     [Table("Product")]
     public class ProductRecord
     {
-        [Key] [Column(TypeName ="nvarchar(150)")] public string Id { get; set; } = "";
+        [Key] public string Id { get; set; }
         [Column(TypeName = "nvarchar(150)")]public string? CategoryName { get; set; }
         [Column(TypeName = "nvarchar(200)")] public string ProductName { get; set; } = "";
         [Column(TypeName = "nvarchar(1000)")] public string? ProductDescription { get; set; } = "";
